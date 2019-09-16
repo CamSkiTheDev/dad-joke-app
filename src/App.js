737ba56571ad1a120d5faf5f31de5c399de34db2
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2019-09-16 14:53:12 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
+import './App.sass';
+import { async } from 'q';
 
-function App() {
+
+export default () => {
+  const [appState, SetAppState] = useState({ isLoading: true, joke: '' });
+
+  const GetJoke = () => {
+    fetch('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json' } })
+      .then(res => res.json())
+      .then(json => SetAppState({ ...appState, joke: json.joke }))
+      .catch(err => console.error(err))
+  }
+
+  useEffect(() => {
+    fetch('https://icanhazdadjoke.com/', { headers: { 'Accept': 'application/json' } })
+      .then(res => res.json())
+      .then(json => SetAppState({ isLoading: false, joke: json.joke }))
+      .catch(err => console.error(err))
+  }, [])
+
+  const { isLoading, joke } = appState;
+
+  if (isLoading) return (<h2>Loading...</h2>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Punny Dad Joke App</h2>
+      <h1>{joke}</h1>
+      <button onClick={GetJoke}>Not Funny?</button>
     </div>
   );
-}
-
-export default App;
+};
